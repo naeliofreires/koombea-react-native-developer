beb31685eb4bebc8023d82e3c98fe71275de5682
Naélio Freires <naeliosistemas@gmail.com>
2024-06-23 11:02:54 -0300

    small code improvement

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,5 @@
 import axios from 'axios';
-
 import Config from '~/config';
-
-export const api = axios.create({
-  baseURL: Config.BASE_URL,
-  timeout: Config.TIMEOUT,
-});
+const baseURL = Config.BASE_URL;
+const timeout = Config.TIMEOUT;
+export const api = axios.create({ baseURL, timeout });
